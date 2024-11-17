626889fbbe3ae2c0d3662da42eeaa033c6fc84c3
Ruddarm <ruddarmmaurya@gmail.com>
2024-11-17 12:07:48 +0530

    register admin

--- libraryManager/catalog/admin.py
+++ libraryManager/catalog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Book,BookInstance,Genre,Author,Language
 
+admin.site.register(Book)
+admin.site.register(BookInstance)
+admin.site.register(Author)
+admin.site.register(Language)
+admin.site.register(Genre)
 # Register your models here.
